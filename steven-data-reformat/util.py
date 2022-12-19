8667319bd020be35fc1fef1c26c627d222fbaa4a
Steven Truong <steventruong@g.ucla.edu>
2022-12-19 15:30:21 -0800

    Default to 60-day averages instead of 1-week averages

--- steven-data-reformat/util.py
+++ steven-data-reformat/util.py
@@ -17,8 +17,7 @@ def aggregate_matches(
     aggregate_fn: Callable[[list[Match], any], any],
     # Default first time is October 3rd, 2022, which is the week before the first tracked customs
     start_date: datetime = datetime(year=2022, month=10, day=3),
-    # Defaults to two-week blocks
-    interval: timedelta = timedelta(weeks=1),
+    interval: timedelta = timedelta(days=60),
 ) -> list[IntervalData]:
     """Aggregate matches into specified intervals. `matches` must be sorted from oldest to newest."""
     out = []
