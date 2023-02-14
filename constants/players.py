cb12f602f383eb7e37803935f42abd2f4836b5a0
stevenktruong <steventruong@g.ucla.edu>
2023-02-13 23:52:20 -0800

    Start tracking Bree

--- constants/players.py
+++ constants/players.py
@@ -1,6 +1,7 @@
 # Player names used as keys in output JSON
 ANDY = "andy"
 BRANDON = "brandon"
+BREE = "bree"
 BRIAN = "brian"
 CADE = "cade"
 DARWIN = "darwin"
@@ -19,12 +20,12 @@ YANG = "yang"
 # Keys for player_info
 NAME = "name"
 VALORANT_TAG = "valorant_tag"
-DISCORD_PIC = "discord_pic"
 
 # Players of interest
 PLAYER_NAMES = [
     ANDY,
     BRANDON,
+    BREE,
     BRIAN,
     CADE,
     DARWIN,
@@ -46,81 +47,69 @@ PLAYER_INFO = {
     ANDY: {
         NAME: "Andy",
         VALORANT_TAG: "Candysan",
-        # DISCORD_PIC: "https://i.ibb.co/x5xzfYQ/disc-pic-andy-circle-250-v2.png",
     },
     BRANDON: {
         NAME: "Brandon",
         VALORANT_TAG: "BigBoiB",
-        # DISCORD_PIC: "https://i.ibb.co/fQWhXhv/disc-pic-brandon-circle-250.png",
+    },
+    BREE: {
+        NAME: "Bree",
+        VALORANT_TAG: "Brioche",
     },
     BRIAN: {
         NAME: "Brian",
         VALORANT_TAG: "brianwoohoo",
-        # DISCORD_PIC: "https://i.ibb.co/tsZ7h2m/disc-pic-brian-circle-250.png",
     },
     CADE: {
         NAME: "Cade",
         VALORANT_TAG: "RhythmKing",
-        # DISCORD_PIC: "https://i.ibb.co/c2S6nRZ/disc-pic-cade-circle-250.png",
     },
     DARWIN: {
         NAME: "Darwin",
         VALORANT_TAG: "ChzGorditaCrunch",
-        # DISCORD_PIC: "https://i.ibb.co/jVR2542/disc-pic-darwin-circle-250.png",
     },
     JOSH: {
         NAME: "Josh",
         VALORANT_TAG: "Bot001341",
-        # DISCORD_PIC: "https://i.ibb.co/nR7WYQV/disc-pic-josh-circle-250.png",
     },
     LINDSEY: {
         NAME: "Lindsey",
         VALORANT_TAG: "aylindsay",
-        # DISCORD_PIC: "https://i.ibb.co/kMZxKMy/disc-pic-lindsey-circle.png",
     },
     SEQUENTIAL: {
         NAME: "Sequential",
         VALORANT_TAG: "sequential",
-        # DISCORD_PIC: "https://i.ibb.co/Mp7kmDR/disc-pic-sequential-circle-250.png",
     },
     SOPHIE: {
         NAME: "Sophie",
         VALORANT_TAG: "chushberry",
-        # DISCORD_PIC: "https://i.ibb.co/qF5gcKh/disc-pic-sophie-circle-250.png",
     },
     STEVE: {
         NAME: "Steve",
         VALORANT_TAG: "Selintt",
-        # DISCORD_PIC: "https://i.ibb.co/0MjK5mG/disc-pic-steve-circle-250.png",
     },
     STEVEN: {
         NAME: "Steven",
         VALORANT_TAG: "youngsmasher",
-        # DISCORD_PIC: "https://i.ibb.co/C5kh1Gb/disc-pic-steven-cricle-250.png",
     },
     SUN: {
         NAME: "Sun",
         VALORANT_TAG: "sun",
-        # DISCORD_PIC: "https://i.ibb.co/MPgJ9cs/disc-pic-sun-circle-250.png",
     },
     SUSI: {
         NAME: "Susi",
         VALORANT_TAG: "SusTwins",
-        # DISCORD_PIC: "https://i.ibb.co/k659mZS/disc-pic-susi-circle-250.png",
     },
     SUSU: {
         NAME: "Susu",
         VALORANT_TAG: "danielscutiegf",
-        # DISCORD_PIC: "https://i.ibb.co/9r0s0Kp/disc-pic-susu-circle-250.png",
     },
     TANG: {
         NAME: "Tang",
         VALORANT_TAG: "tangy",
-        # DISCORD_PIC: "https://i.ibb.co/GCSsLK7/disc-pic-tang-circle-250.png",
     },
     YANG: {
         NAME: "Yang",
         VALORANT_TAG: "Tyblerone",
-        # DISCORD_PIC: "https://i.ibb.co/kxFc1ng/disc-pic-yang-circle-250.png",
     },
 }
