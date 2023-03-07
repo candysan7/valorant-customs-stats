f339334c4dd9d05c252e6f322cc23132c90c676b
stevenktruong <steventruong@g.ucla.edu>
2023-03-07 02:30:04 -0800

    out-min is now only for API-related JSON files and out is for human-readable ones

--- generate_datasets.py
+++ generate_datasets.py
@@ -30,14 +30,18 @@ def generate_datasets(output_dir, minified=False):
     ]
     wallOfShameGenerator = WallOfShameGenerator()
 
-    for match in matches:
-        for generator in dashboardGenerators:
-            generator.accumulate(match)
-        wallOfShameGenerator.accumulate(match)
+    all_generators = dashboardGenerators + [wallOfShameGenerator]
 
+    for match in matches:
+        for generator in all_generators:
+            generator.accumulate(match)
+
+    # Data for api/dashboard
     dashboard_json = {}
     for generator in dashboardGenerators:
         dashboard_json[generator.name] = generator.finalize(minified=minified)
+
+    # Data for api/wall-of-shame
     wallOfShameGenerator.finalize()
     wallOfShameGenerator.generate(output_dir=output_dir, minified=minified)
 
@@ -58,9 +62,44 @@ def generate_datasets(output_dir, minified=False):
 
 
 if __name__ == "__main__":
-    minified = False
-    output_dir = "./out"
     if len(sys.argv) > 1 and sys.argv[1] == "--minified":
-        minified = True
-        output_dir = "./out-min"
-    generate_datasets(output_dir=output_dir, minified=minified)
+        generate_datasets(output_dir="./out-min", minified=True)
+        exit()
+
+    output_dir = "./out"
+
+    matches: list[Match] = []
+    with open("./data.json", mode="r") as f:
+        data = json.load(f)
+        # data.json is sorted in process_scrape.py
+        # matches = sorted([Match(match_json) for match_json in data], key=lambda m: m.time)
+        matches = [Match(match_json) for match_json in data]
+        f.close()
+
+    generators: list[DatasetGenerator] = [
+        AssistsGivenPerStandardGameGenerator(),
+        AssistsReceivedPerStandardGameGenerator(),
+        EasiestMatchupsGenerator(),
+        IndividualGenerator(),
+        MapsGenerator(),
+        MetaGenerator(),
+        RecentLobbyWinRatesGenerator(),
+        RolesGenerator(),
+        RunningWinrateOverTimeGenerator(),
+        TeamSynergyDataGenerator(),
+        TeammateSynergyGenerator(),
+        WallOfShameGenerator(),
+    ]
+
+    for match in matches:
+        for generator in generators:
+            generator.accumulate(match)
+
+    for generator in generators:
+        generator.finalize()
+        generator.generate(output_dir=output_dir)
+
+    with open(os.path.join(output_dir, "data-frame-friendly.json"), mode="w") as f:
+        out_json = {i: match_json for i, match_json in enumerate(data)}
+        json.dump(out_json, f)
+        f.close()
