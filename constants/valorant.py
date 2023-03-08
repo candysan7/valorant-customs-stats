f40f93154598aa1a36eb7afb0f1a6125d0a4b8b1
stevenktruong <steventruong@g.ucla.edu>
2023-03-08 00:51:47 -0800

    Add Gekko and remove links to images

--- constants/valorant.py
+++ constants/valorant.py
@@ -16,6 +16,7 @@ BRIMSTONE = "Brimstone"
 CHAMBER = "Chamber"
 CYPHER = "Cypher"
 FADE = "Fade"
+GEKKO = "Gekko"
 HARBOR = "Harbor"
 JETT = "Jett"
 KAYO = "KAY/O"
@@ -55,6 +56,7 @@ AGENT_NAMES = [
     CHAMBER,
     CYPHER,
     FADE,
+    GEKKO,
     HARBOR,
     JETT,
     KAYO,
@@ -73,29 +75,6 @@ AGENT_NAMES = [
 
 ROLE_NAMES = [CONTROLLER, DUELIST, INITIATOR, SENTINEL]
 
-AGENT_NAME_TO_FULL_BODY_IMAGE_URL = {
-    ASTRA: "https://i.ibb.co/tpFTDZP/agent-astra.png",
-    BREACH: "https://i.ibb.co/GtG7Z01/agent-breach.png",
-    BRIMSTONE: "https://i.ibb.co/RCygQtF/agent-brimstone.png",
-    CHAMBER: "https://i.ibb.co/wwW5pw9/agent-chamber.png",
-    CYPHER: "https://i.ibb.co/47jc4bW/agent-cypher.png",
-    FADE: "https://i.ibb.co/HpQ9nmj/agent-fade.png",
-    HARBOR: "https://i.ibb.co/cQWBVhF/agent-harbor.png",
-    JETT: "https://i.ibb.co/jV79G9x/agent-jett.png",
-    KAYO: "https://i.ibb.co/zGxz9WH/agent-kayo.png",
-    KILLJOY: "https://i.ibb.co/sVZBy9y/agent-killjoy.png",
-    NEON: "https://i.ibb.co/kX8npRV/agent-neon.png",
-    OMEN: "https://i.ibb.co/gyF9RPn/agent-omen.png",
-    PHOENIX: "https://i.ibb.co/jr62DmS/agent-phoenix.png",
-    RAZE: "https://i.ibb.co/m84byGB/agent-raze.png",
-    REYNA: "https://i.ibb.co/Q99BLjb/agent-reyna.png",
-    SAGE: "https://i.ibb.co/3Bjc4Ff/agent-sage.png",
-    SKYE: "https://i.ibb.co/YWDbNSd/agent-skye.png",
-    SOVA: "https://i.ibb.co/7nhNYq3/agent-sova.png",
-    VIPER: "https://i.ibb.co/Kxd71pz/agent-viper.png",
-    YORU: "https://i.ibb.co/fC5Kcq1/agent-yoru.png",
-}
-
 AGENT_NAME_TO_ROLE = {
     ASTRA: CONTROLLER,
     BREACH: INITIATOR,
@@ -103,6 +82,7 @@ AGENT_NAME_TO_ROLE = {
     CHAMBER: SENTINEL,
     CYPHER: SENTINEL,
     FADE: INITIATOR,
+    GEKKO: INITIATOR,
     HARBOR: CONTROLLER,
     JETT: DUELIST,
     KAYO: INITIATOR,
